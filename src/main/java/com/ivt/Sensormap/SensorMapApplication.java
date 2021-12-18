03cb1081a2083136ea06a6ee6d1b3b6faf661880
Kriispiii <70846965+Kriispiii@users.noreply.github.com>
2021-12-18 18:46:51 +0300

    v1.1

---
+++ src/main/java/com/ivt/Sensormap/SensorMapApplication.java
@@ -0,0 +1,13 @@
+package com.ivt.Sensormap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivt/Sensormap/SensorMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivt.Sensormap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
