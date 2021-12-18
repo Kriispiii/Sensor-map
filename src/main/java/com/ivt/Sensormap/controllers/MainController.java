47b144255be8a42011ae7b77703936ca72cb0d11
Kriispiii <70846965+Kriispiii@users.noreply.github.com>
2021-12-18 18:45:51 +0300

    v1.0

---
+++ src/main/java/com/ivt/Sensormap/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.ivt.Sensormap.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("sensor-map", "Main");
+        return "main";
+    }
+
+}
